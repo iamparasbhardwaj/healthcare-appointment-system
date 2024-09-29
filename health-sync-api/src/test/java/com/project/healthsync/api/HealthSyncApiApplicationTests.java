ae37faf8b49af3f98a5a1cb06a455a6615025186
Paras <32291716+iamparasbhardwaj@users.noreply.github.com>
2024-09-29 13:51:38 +0530

    Health Sync Api
    Health Sync API project initial setup.

---
+++ health-sync-api/src/test/java/com/project/healthsync/api/HealthSyncApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.health_sync_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthSyncApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
